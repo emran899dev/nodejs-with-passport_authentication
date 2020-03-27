028dbd43f77eaeffc81da5b4dcb55f814fe1f21f
Md Emran Nazir <mdemrannazir9999@gmail.com>
2020-03-27 06:37:10 +0600

    ready to upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+//DB Connect
+mongoose.connect('mongodb://localhost/passport-login', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+});
+
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  });
+
+// Router 
+app.use('/', require('./router/index'));
+app.use('/users', require('./router/user'));
+
+//Port
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started port on ${PORT}`));
